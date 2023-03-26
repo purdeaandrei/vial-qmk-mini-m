a18199fbb4a74688920a989f632421c37cca8cc4
Purdea Andrei <andrei@purdea.ro>
2023-03-26 04:17:25 +0300

    vial

---
+++ keyboards/unicomp/mini_m/keymaps/vial/config.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#undef SERIAL_NUMBER
+#define SERIAL_NUMBER "purdea.ro:minirazz:vial:f64c2b3c"
+
+#define VIAL_KEYBOARD_UID {0x49, 0x7B, 0x5D, 0x85, 0x5F, 0xD1, 0x6E, 0xA7}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 1, 0xd }
+
+#define DYNAMIC_KEYMAP_MACRO_COUNT 32
+#define DYNAMIC_KEYMAP_LAYER_COUNT 16
